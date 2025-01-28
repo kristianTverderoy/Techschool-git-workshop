4fa96019520e5fb60354c2e146e63c76bd13090a
Kristian Tverderøy <kristianty80@gmail.com>
2025-01-28 18:43:47 +0100

    Merge conflict test

--- index.ts
+++ index.ts
@@ -8,3 +8,15 @@ export const greeting = (firstname: string, lastname: string) => {
     console.log(`Hello ${firstname} ${lastname}`);
 };
 
+const people = [
+    { firstname: "Arne", lastname: "Arnesen" },
+    { firstname: "Magnus", lastname: "Magnusson" },
+    { firstname: "Siri", lastname: "Sirisen" },
+    { firstname: "Stefan", lastname: "Stefanson" },
+];
+
+export const greetTheGang = () => {
+    people.forEach((person) => {
+        greeting(person.firstname, person.lastname);
+    });
+};
