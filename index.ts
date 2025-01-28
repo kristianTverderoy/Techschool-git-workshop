58c2caa31360f0072541c5f389bb0d8ba4f4dddd
Kristian Tverderøy <kristianty80@gmail.com>
2025-01-28 18:36:20 +0100

    index.ts commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+/**
+ * Function that greets a person
+ * @param firstname First name
+ * @param lastname Last name
+ */
+
+export const greeting = (firstname: string, lastname: string) => {
+    console.log(`Hello ${firstname} ${lastname}`);
+};
+
